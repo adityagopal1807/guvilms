5785e4d1b9f218a64cfad6cb98c2341dec04c202
adityagopal1807 <160997178+adityagopal1807@users.noreply.github.com>
2024-12-31 19:20:30 +0530

    Create enrollment.sql

---
+++ learning management system/srcmain/SQL/enrollment.sql
@@ -0,0 +1,7 @@
+CREATE TABLE enrollments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
